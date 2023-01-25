8b814a5e09f6b2b50085b09f6dd6d4b01089a5b6
Martin Gonzalez <martineluney023@gmail.com>
2023-01-25 19:26:45 -0300

    M3U3

---
+++ M2-U3-4/routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer')
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', async (req, res, next) => {
+
+  console.log(req.body)
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var mensaje = req.body.mensaje
+
+  var obj = {
+    to: 'martineluney023@gmail.com'
+  subject= 'Contacto desde la web'
+  html: nombre + "" + apellido + "se contacto con nosotros a traves de la web y su email es" + email + ". <br> A demas, hizo el siguiente comentario"
+  }
+
+  var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  })
+
+  var info = await transporter.sendMail(obj);
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente'
+  });
+});
+
+
+module.exports = router;
